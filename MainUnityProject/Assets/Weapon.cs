f2cf7701f9d682d4300901f3cf97d73bee72c9be
Programmering <teknisk+programmering-GDC2025-GR5@game.unf.dk>
2025-07-13 08:56:43 +0200

    weapon stuff

---
+++ MainUnityProject/Assets/Weapon.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+[System.Serializable]
+
+enum StatType {attackDamage, attackSpeed, critDamageMult, Critchance}
+
+public class Weapon
+{
+    
+    public int attackDamage;
+    public float attackSpeed;
+    public float critDamageMult;
+    public float critChance;
+
+  
+
+}
---
+++ MainUnityProject/Assets/WeaponManagerScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class WeaponManagerScript : MonoBehaviour
+{
+    public GameObject Ballista;
+    public GameObject Canon;
+    public GameObject Catapult;
+    
+    
+    
+    
+}
