65cf949dcc00c0223720a7373a07301a2115c763
Programmering <teknisk+programmering-GDC2025-GR5@game.unf.dk>
2025-07-13 10:22:35 +0200

    made weapon script and all weapons sub scripts

--- MainUnityProject/Assets/Weapon.cs
+++ MainUnityProject/Assets/Weapon.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+
 [System.Serializable]
 
-enum StatType {attackDamage, attackSpeed, critDamageMult, Critchance}
+public enum StatType {attackDamage, attackSpeed, critDamageMult, Critchance}
+public enum WeaponType {Ballista, Canon, Catapult}
 
-public class Weapon
+public class Weapon : MonoBehaviour
 {
-    
+    public WeaponType weaponType;
     public int attackDamage;
     public float attackSpeed;
     public float critDamageMult;
     public float critChance;
+    
+    //upgrade values
+    [Header("Increase amounts for stats")]
+    public int atkDamageIncreaseAmount;
+    public float atkSpeedIncreaseAmount;
+    public float critDamageMultIncreaseAmount;
+    public float critChanceIncreaseAmount;
 
-  
+    
+    public void UpgradeStat(StatType statType)
+    {
 
+
+        switch (statType)
+        {
+            case StatType.attackDamage:
+                attackDamage += atkDamageIncreaseAmount;
+                break;
+            case StatType.attackSpeed:
+                attackSpeed += atkSpeedIncreaseAmount;
+                break;
+            case StatType.critDamageMult:
+                critDamageMult += critDamageMultIncreaseAmount;
+                break;
+            case StatType.Critchance:
+                critChance += critChanceIncreaseAmount;
+                break;
+            default:
+                Debug.Log("No stat could be upgraded");
+                break;
+
+        }
+    }
 }
